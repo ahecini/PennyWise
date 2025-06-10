70e1d374f2047e2ee7dc06bf3eb3646430f33e76
ahecini <hecini.abdeldjalil@gmail.com>
2025-06-10 22:08:44 +0200

    Signup post-SQL V1

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ class Database:
         self.conn = sqlite3.connect('example.db')  # Creates a new database file if it doesn’t exist
         self.conn.execute("PRAGMA foreign_keys = 1")
         self.cursor = self.conn.cursor()
-    def createDatabase(self):
+    def __createDatabase(self):
         self.cursor.executescript("""
                     create table `user`( 
                         username varchar(50) PRIMARY KEY,
@@ -38,7 +38,7 @@ class Database:
                     );
                     """)
         self.conn.commit()
-    def dropDatabase(self):
+    def __dropDatabase(self):
         self.cursor.executescript("""
                      drop table budget;
                      drop table `transaction`;
@@ -46,6 +46,9 @@ class Database:
                      drop table user;
                     """)
         self.conn.commit()
+    def startOver(self):
+        self.__dropDatabase()
+        self.__createDatabase()
     def insertUser(self, data):
         self.cursor.execute("insert into user values (?,?,?)", data)
         self.conn.commit()
@@ -81,8 +84,6 @@ class Login(tk.Frame):
         if self.db.isUserExist((self.text1.get(),self.text2.get())) :
             background.tkraise()
             frame.tkraise()
-            self.text1['text'] = " "
-            self.text2['text'] = " "
         else:
             messagebox.showinfo("Failure", "Username not found!")
     def signup(self, frame, background):
@@ -95,6 +96,7 @@ class Login(tk.Frame):
 class Signup(tk.Frame):
     def __init__(self, root):
         super().__init__(root, width=300, height=200) #150
+        self.db = Database()
         self.config(bg="#4B41D7")
         self.place(x=50,y=85) #115
         self.label1 = ttk.Label(self, text="user", font=('Segoe UI', 12), background="#4B41D7")
@@ -110,12 +112,22 @@ class Signup(tk.Frame):
         self.text3 = ttk.Entry(self, font=('Helvetica',8), width=40, bootstyle="info")
         self.text3.place(x=16,y=120.5)
         self.login_button = ttk.Button(self, text="Login", bootstyle="success", width=20)
-        #self.login_button.place(x=16,y=155.5) #115.5
+        self.login_button.place(x=16,y=155.5) #115.5
         self.signup_button = ttk.Button(self, text="Sign up", bootstyle="info", width=13)
         self.signup_button.place(x=170,y=155.5) #115.5
     def setSignupButtonCommand(self, frame, background):
-        self.signup_button['command'] = lambda:self.raise_frame(frame, background)
-    def raise_frame(self, frame, background):
+        self.signup_button['command'] = lambda:self.signup(frame, background)
+    def setLoginButtonCommand(self, frame, background):
+        self.login_button['command'] = lambda:self.login(frame, background)
+    def signup(self, frame, background):
+        try:
+            data = (self.text1.get(), self.text2.get(), float(self.text3.get()))
+            self.db.insertUser(data)
+            background.tkraise()
+            frame.tkraise()
+        except:
+            messagebox.showinfo("Failure", "Could not sign up!")
+    def login(self, frame, background):
         background.tkraise()
         frame.tkraise()
     '''
@@ -155,11 +167,14 @@ class Main(tk.Tk):
         self.frame1.setLoginButtonCommand(self.frame2,self.frame_background)
         self.frame2.setBackButtonCommand(self.frame1,self.frame_background)
         self.frame3.setSignupButtonCommand(self.frame1,self.frame_background)
+        self.frame3.setLoginButtonCommand(self.frame1,self.frame_background)
         self.frame1.setSignupButtonCommand(self.frame3,self.frame_background)
 if __name__ == "__main__":
     root = Main()
     root.mainloop()   
     """
     db = Database()
-    print(db.isUserExist(('wildcat6','xxx')))
+    db.startOver()
     """
+    #print(db.isUserExist(('wildcat6','xxx')))
+
